0786e7484d192df68f0cbd09a260a37cf810eb08
kiilkim <58096214+kiilkim@users.noreply.github.com>
2021-03-18 13:14:36 +0900

    initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,30 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('polls/', include('polls.urls')), #url parsing
+    path('admin/', admin.site.urls),
+]
+
+#include 함수로 polls.urls 참고
+
+#최상위 conf에서 참고 
+
+#url parsing 해서 잡아내고? polls app 에선 전달받으면 view 내부로 index 로 연결 index엔 hello~가있다.
+
+
